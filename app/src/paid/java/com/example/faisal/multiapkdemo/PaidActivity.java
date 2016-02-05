43d2b6afafee929ae397aa5da2ac591eef75f154
Faisal Khan <faisalkhan1690@gmail.com>
2016-02-05 15:15:30 +0530

    Initial commit

---
+++ app/src/paid/java/com/example/faisal/multiapkdemo/PaidActivity.java
@@ -0,0 +1,13 @@
+package com.example.faisal.multiapkdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PaidActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_paid);
+    }
+}
